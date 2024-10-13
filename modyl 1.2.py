bf55b3b30e137b73b66f59d067fb4492c16ac4e1
Юлия <veberulia530@gmail.com>
2024-10-13 16:17:55 +0300

    Add files via upload

---
+++ modyl 1.2.py
@@ -0,0 +1,5 @@
+quantity_vipolnen_dz="12"
+quantity_zatrahcen_hour="1.5"
+nazvanie_kursa="Python"
+time_na_one_zadanie="0.125"
+print("Курс",nazvanie_kursa,",всего задач:",quantity_vipolnen_dz,",затрачено часов:",quantity_zatrahcen_hour,",среднее время выполнения:",time_na_one_zadanie,"часа.")
